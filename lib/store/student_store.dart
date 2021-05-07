dba6a50b3f436b083159beb7ee93b03d261ceae5
giaolinh99 <giaolinhdo.com>
2021-05-07 17:19:53 +0700

    baitap7

---
+++ lib/store/student_store.dart
@@ -0,0 +1,51 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:lecture2/models/sinhvien.dart';
+import 'package:mobx/mobx.dart';
+
+import '../Student_api.dart';
+
+part 'student_store.g.dart';
+
+class StudentStore = StudentStoreBase with _$StudentStore;
+
+abstract class StudentStoreBase with Store {
+  StudentApi studentApi = StudentApi();
+  ReactionDisposer disposer;
+  var pageController = PageController();
+
+  void runReactionDisposer() {
+    disposer = reaction((_) => listType, (result) {
+      return result;
+    });
+  }
+
+  void releaseReaction() {
+    disposer();
+  }
+
+  @observable
+  ObservableFuture<List<SinhVien>> students;
+  bool listType = false;
+  int selectedIndex = 0;
+
+  @action
+  Future fetchStudents() => students =
+      ObservableFuture(studentApi.getPost().then((students) => students));
+
+  void getStudents() {
+    fetchStudents();
+  }
+
+  @action
+  void changePage(int index) {
+    pageController.jumpToPage(index);
+    selectedIndex = index;
+  }
+
+  @action
+  void changeListType() {
+    listType = !listType;
+  }
+}
